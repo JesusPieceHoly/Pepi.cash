732f342e85e5e9b91dfd24118e1dedb9fde68e07
JesusPieceHoly <116229206+JesusPieceHoly@users.noreply.github.com>
2024-01-03 01:16:11 -0500

    Update mintingParams.js

--- js/mintingParams.js
+++ js/mintingParams.js
@@ -1,25 +1,25 @@
 // Configure minting params
 
 // Wallet Connect projectId
-const projectId = "2aca272d18deb10ff748260da5f78bfd";
+const projectId = "ee25137ab77cffa284900841206a8e8f";
 
 // Url of the API server
-const urlApiServer = "https://api.ninjas.cash";
+const urlApiServer = "https://api.pepi.cash";
 
 // Contract Params mint
 const tokenId = "77a95410a07c2392c340384aef323aea902ebfa698a35815c4ef100062c6d8ac";
-const collectionSize = 5_000;
+const collectionSize = 7_777;
 const numberOfThreads = 25;
-const mintPriceSats = 5_000_000;
+const mintPriceSats = 1_450_000;
 const payoutAddress = "bitcoincash:qqds0h006djrnast7ktvf7y3lrmvu0d7yqzhuzgvaa"; // with bitcoincash: or bchtest: prefix
 const network = "mainnet";
 
 // Wallet Connect Metadata
 const wcMetadata = {
-  name: 'Cash-Ninjas',
+  name: 'Pepi Cash Token',
   description: 'CashTokens NFT Collection',
-  url: 'https://ninjas.cash/',
-  icons: ['https://ninjas.cash/images/logo.png']
+  url: 'https://pepi.cash/',
+  icons: ['https://pepi.cash/images/logo.png']
 };
 
 export { projectId, urlApiServer, tokenId, collectionSize, mintPriceSats, payoutAddress, numberOfThreads, network, wcMetadata };
