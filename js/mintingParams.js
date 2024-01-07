6f0c25e149f69562ad70a9e07f6f034b9e5ad846
JesusPieceHoly <116229206+JesusPieceHoly@users.noreply.github.com>
2024-01-07 00:59:32 -0500

    Update mintingParams.js

--- js/mintingParams.js
+++ js/mintingParams.js
@@ -7,9 +7,9 @@ const projectId = "ee25137ab77cffa284900841206a8e8f";
 const urlApiServer = "https://api.pepi.cash";
 
 // Contract Params mint
-const tokenId = "cd10870dab23ac5451e23381b35c1691049f77091463a1f98995d2551a052184";
+const tokenId = "5deaf71fcce24212949e0d5120adb242ad826ade9169c57ebd0970812cd3a598";
 const collectionSize = 7_777;
-const numberOfThreads = 20;
+const numberOfThreads = 10;
 const mintPriceSats = 1_450_000;
 const payoutAddress = "bitcoincash:qzm5qumtmmdf49tx4gwn85yyv6wm4efyqqlzhyd8sg"; // with bitcoincash: or bchtest: prefix
 const network = "mainnet";
@@ -17,7 +17,7 @@ const network = "mainnet";
 // Wallet Connect Metadata
 const wcMetadata = {
   name: 'Pepi Cash Token',
-  description: 'CashTokens NFT Collection',
+  description: 'The original Meme frog on Cash Tokens BCH',
   url: 'https://pepi.cash/',
   icons: ['https://pepi.cash/images/logo.png']
 };
